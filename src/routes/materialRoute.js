76cfed7e9672f63e39e1566b4eb60a06aafbb4bd
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-06-27 07:47:23 +0530

    feat: Add file upload handling to create and update material routes - Updated the POST and PUT routes for  in the router. - Integrated multer middleware to handle single file uploads for .

--- src/routes/materialRoute.js
+++ src/routes/materialRoute.js
@@ -1,9 +1,19 @@
 const Controllers = require('../controllers')
+const upload = require('../middlewares/multer')
 
 module.exports = (router) => {
-    router.get('/materials', Controllers.materialsControllers.getAllMaterials)
-    router.get('/materials/:id', Controllers.materialsControllers.getMaterialById)
-    router.post('/materials', Controllers.materialsControllers.createMaterial)
-    router.put('/materials/:id', Controllers.materialsControllers.updateMaterial)
-    router.delete('/materials/:id', Controllers.materialsControllers.deleteMaterial)
+    router
+        .get('/materials', Controllers.materialsControllers.getAllMaterials)
+        .get('/materials/:id', Controllers.materialsControllers.getMaterialById)
+        .post(
+            '/materials',
+            upload.single('imageUrl'),
+            Controllers.materialsControllers.createMaterial,
+        )
+        .put(
+            '/materials/:id',
+            upload.single('imageUrl'),
+            Controllers.materialsControllers.updateMaterial,
+        )
+        .delete('/materials/:id', Controllers.materialsControllers.deleteMaterial)
 }
