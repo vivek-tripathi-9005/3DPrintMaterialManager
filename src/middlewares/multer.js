4048e7991bc1c82278361d492b193dcdaa940794
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-06-27 07:38:37 +0530

    feat: Add multer middleware for file uploads - Added multer configuration for handling file uploads. - Ensured the existence of the uploads directory by creating it if necessary. - Defined a storage engine with a custom destination and filename for uploaded files. - Implemented a file filter to restrict uploads to JPEG, JPG, and PNG file types only. - Set a file size limit of 5MB for uploads.

---
+++ src/middlewares/multer.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+
+// Ensure uploads directory exists
+const ensureUploadsDir = (dir) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+  };
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const uploarDirectory = './static/uploads'
+        ensureUploadsDir(uploarDirectory)
+        cb(null, uploarDirectory)
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${path.basename(file.originalname)}`);
+    },
+})
+
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+  if (allowedFileTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type. Only JPEG, JPG, and PNG files are allowed.'), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 }, // 5MB limit
+});
+
+module.exports = upload;
